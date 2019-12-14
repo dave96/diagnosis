f789e317e8dc00e0566f99c169e3e8f5b882c294
Mario Rodriguez <ich.sphon@gmail.com>
2019-12-14 19:43:11 +0100

    XML Parsing

--- backend/diagnosis/models.py
+++ backend/diagnosis/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Disorder(models.Model):
+    identifier = models.IntegerField()
+    orphanumber = models.CharField(max_length=120,unique=True)
+    name = models.CharField(max_length=120)
+
+class Phenotype(models.Model):
+    identifier = models.IntegerField()
+    term = models.CharField(max_length=120)
+    orphanumber = models.CharField(max_length=120,unique=True)
+
+class Frequency(models.Model):
+    orphanumber = models.CharField(max_length=120,unique=True)
+    text = models.CharField(max_length=120)
+
+
+class Disorder_Phenotype(models.Model):
+    disorder_orphanumber = models.ForeignKey(Disorder, on_delete=models.CASCADE)
+    phenotype_orphanumber = models.ForeignKey(Phenotype, on_delete=models.CASCADE)
+    frequency_orphanumber = models.ForeignKey(Frequency , on_delete=models.CASCADE)
